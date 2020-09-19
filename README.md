86bf835af8a64df92f14a1e4f80796d5d6b7b189
Tkaixiang <tkaixiang@gmail.com>
2020-09-19 12:38:47 +0800

    Updates

--- README.md
+++
@@ -1,1 +0,0 @@
-Personal GitHub pages website. Visit at: https://tkaixiang.github.io/website/
---
+++ precache-manifest.bf57d9941240d106f4b93f3d50975719.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfb15a7cc46c37abbb8fda2fef6def69",
+    "url": "/index.html"
+  },
+  {
+    "revision": "abc04fc2d7404217b2ad",
+    "url": "/static/css/main.b0c84e20.chunk.css"
+  },
+  {
+    "revision": "886cc1f8116b0a487eac",
+    "url": "/static/js/2.49ebea5d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.49ebea5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abc04fc2d7404217b2ad",
+    "url": "/static/js/main.9f93aaa7.chunk.js"
+  },
+  {
+    "revision": "01f799ed6627141d759f",
+    "url": "/static/js/runtime-main.4949111c.js"
+  },
+  {
+    "revision": "79297687bec0fd16d74fce1928cc1647",
+    "url": "/static/media/Map.79297687.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
